2974fef45f7eeca7384ad4c738de135317d13e33
pdrolucasz <pedro.lucas4431@gmail.com>
2021-01-08 00:44:11 -0300

    estilização de sidebar para dashboard

--- web/src/components/Sidebar/index.tsx
+++ web/src/components/Sidebar/index.tsx
@@ -1,22 +1,40 @@
 import React from 'react';
-import { FiArrowLeft } from 'react-icons/fi';
+import { FiArrowLeft, FiPower } from 'react-icons/fi';
 import { useHistory } from "react-router-dom";
 
 import mapMarkerImg from '../../images/map-marker.svg';
 
+import { useAuth } from '../../hooks/auth'
+
 import './styles.css'
 
-const Sidebar: React.FC = () => {
+interface SidebarProps {
+    dashboard?: boolean
+}
+
+const Sidebar: React.FC<SidebarProps> = ({ dashboard }) => {
     const { goBack } = useHistory();
+    const { signOut } = useAuth()
 
     return (
         <aside className="app-sidebar">
             <img src={mapMarkerImg} alt="Happy" />
 
+            {/*<main>
+                <FiMapPin size={24} color="#FFF" />
+            </main>*/}
+
             <footer>
-            <button type="button" onClick={goBack}>
-                <FiArrowLeft size={24} color="#FFF" />
-            </button>
+
+                {dashboard ? 
+                    <button type="button" onClick={signOut}>
+                        <FiPower size={24} color="#FFF" />
+                    </button>
+                    :
+                    <button type="button" onClick={goBack}>
+                        <FiArrowLeft size={24} color="#FFF" />
+                    </button>
+                }
             </footer>
         </aside>
     )
